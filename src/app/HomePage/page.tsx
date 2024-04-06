00b13202f369686ef4fe013034065fbad890122a
Bom <p.atipattranan@gmail.com>
2024-04-06 13:49:47 +0700

    add Pantip Pick component

--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -1,13 +1,15 @@
 import {FC} from "react";
 import TabRooms from "../../components/TabRooms"
 import Highlight from "../../components/Highlight"
+import PantipPick from "@/components/PantipPick";
 
-const HomePage : FC = () => {
-  return (
-      <>
-        <Highlight/>
-      <TabRooms/>
-      </>
-  )
+const HomePage: FC = () => {
+    return (
+        <>
+            <Highlight/>
+            <PantipPick/>
+            <TabRooms/>
+        </>
+    )
 }
 export default HomePage
